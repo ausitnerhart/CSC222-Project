8a9940e398ee8180d22a520fd9f0268f1ced3662
Austin <austinerhartm@gmail.com>
2023-02-16 13:16:13 -0600

    proper form of the code

---
+++ main.c
@@ -0,0 +1,124 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <stdbool.h>
+
+const bool DEBUG = true;
+const int MAX_LEN = 256;
+
+char* CommandPrompt(){
+	char cwd[MAX_LEN];
+	char* input = malloc(MAX_LEN * sizeof(char));
+
+	getcwd(cwd, sizeof(cwd));
+	printf("%s $ ", cwd);
+
+	if(fgets(input, MAX_LEN, stdin) == NULL) {
+		return NULL;
+	}
+
+	input[strcspn(input, "\n")] = 0;
+	return input;
+}
+
+struct ShellCommand{
+	int argc;
+	char** argv;
+	int inputRedirect;
+	char* inputFile;
+	int outputRedirect;
+	char* outputFile;
+};
+
+struct ShellCommand ParseCommandLine(char* input){
+
+	struct ShellCommand sc = {0};
+	char* token;
+	char* nextToken;
+	int i;
+
+	sc.argc = 0;
+	sc.argv = (char**) malloc(sizeof(char*));
+	sc.inputRedirect = 0;
+	sc.inputFile = NULL;
+	sc.outputRedirect = 0;
+	sc.outputFile = NULL;
+
+	token = strtok(input, " \t");
+	while(token != NULL){
+		if(strcmp(token, "<") == 0){
+			sc.inputRedirect = 1;
+			nextToken = strtok(NULL, " \t");
+			if(nextToken == NULL){
+				fprintf(stderr, "Error no input file specified \n");
+				exit(0);
+			}
+			sc.inputFile = strdup(nextToken);
+		} else if(strcmp(token, ">") == 0){
+			sc.outputRedirect = 1;
+			nextToken = strtok(NULL, " \t");
+			if(nextToken == NULL){
+				nextToken = "a.out";
+			}
+			sc.outputFile = strdup(nextToken);
+		} else{
+			sc.argv[sc.argc] = strdup(token);
+			sc.argc++;
+			sc.argv = (char**)realloc(sc.argv, (sc.argc + 1) * sizeof(char*));
+		}
+
+		sc.argv[sc.argc] = NULL;
+
+	}
+	return sc;
+}
+
+void ExecuteCommand(struct ShellCommand command){
+	pid_t pid = fork();
+	int status;
+
+	if(pid == 0){
+		// Child process
+		if(command.inputRedirect){
+			FILE* fileIn = fopen(command.inputFile, "r");
+			dup2(fileno(fileIn), 0);
+			fclose(fileIn);
+		}
+
+		if(command.outputRedirect){
+		}
+
+		execvp(command.argv[0], command.argv);
+		perror("Error executing command");
+		exit(1);
+	} else if(pid > 0){
+		//Parent process
+		waitpid(pid, &status, 0);
+	} else{
+		//Forking error
+		perror("Error forking process");
+		exit(1);
+	}
+
+}
+
+
+
+
+int main(){
+	char* input;
+	struct ShellCommand command;
+
+	while(true){
+		input = CommandPrompt();
+
+		command = ParseCommandLine(input);
+		free(input);
+
+		ExecuteCommand(command);
+	}
+
+	exit(0);
+}
